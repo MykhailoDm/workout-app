dc9ed5c496283f1400d95d8fa9294916476ab5f2
MykhailoDm <mikhailo2002dm@gmail.com>
2023-02-06 21:41:22 +0200

    Implement sounds for the app

--- app/src/main/java/com/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/workoutapp/ExerciseActivity.kt
@@ -1,15 +1,21 @@
 package com.workoutapp
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.workoutapp.const.Constants
 import com.workoutapp.databinding.ActivityExerciseBinding
 import com.workoutapp.model.ExerciseModel
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding: ActivityExerciseBinding? = null
 
@@ -22,11 +28,16 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
 
+    private var tts: TextToSpeech? = null
+    private var player: MediaPlayer? = null
+
     companion object {
         private const val REST_MAX_TIME = 10000L
         private const val REST_COUNT_DOWN_INTERVAL = 1000L
         private const val EXERCISE_MAX_TIME = 30000L
         private const val EXERCISE_COUNT_DOWN_INTERVAL = 1000L
+        private const val TAG = "ExerciseActivity"
+        private const val APP_RESOURCE_URI_BASE = "android.resource://com.workoutapp/"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +52,8 @@ class ExerciseActivity : AppCompatActivity() {
 
         exerciseList = Constants.defaultExerciseList()
 
+        tts = TextToSpeech(this, this)
+
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
@@ -49,16 +62,30 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupRestView() {
+
+        try {
+            val soundURI = Uri.parse(APP_RESOURCE_URI_BASE + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         binding?.flProgressBarRest?.visibility = View.VISIBLE
         binding?.tvTitle?.visibility = View.VISIBLE
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flProgressBarExercise?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvUpcomingExerciseName?.visibility = View.VISIBLE
 
         if (restTimer != null) {
             restTimer?.cancel()
             restProgress = 0
         }
+
+        binding?.tvUpcomingExerciseName?.text = exerciseList!![currentExercisePosition + 1].name
 
         setRestProgressBar()
     }
@@ -87,11 +114,15 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flProgressBarExercise?.visibility = View.VISIBLE
         binding?.ivImage?.visibility = View.VISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvUpcomingExerciseName?.visibility = View.INVISIBLE
 
         if (exerciseTimer != null) {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].name)
 
         val currentExercise = exerciseList!![currentExercisePosition]
         binding?.ivImage?.setImageResource(currentExercise.image)
@@ -134,6 +165,30 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProgress = 0
         }
 
+        tts?.let {
+            it.stop()
+            it.shutdown()
+        }
+
+        player?.stop()
+
         binding = null
+    }
+
+    private fun speakOut(text: String) {
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e(TAG, "onInit: The Language specified is not supported!")
+            }
+        } else {
+            Log.e(TAG, "onInit:  Initialization failed!")
+        }
     }
 }
