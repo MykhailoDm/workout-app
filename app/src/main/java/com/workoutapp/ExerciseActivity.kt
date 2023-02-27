1f7f1866f0358663480e907a0a553926e614fb36
MykhailoDm <mikhailo2002dm@gmail.com>
2023-02-27 21:49:25 +0200

    Implemented dialog for going back

--- app/src/main/java/com/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/workoutapp/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.workoutapp
 
+import android.app.Dialog
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
@@ -14,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.workoutapp.adapter.ExerciseStatusAdapter
 import com.workoutapp.const.Constants
 import com.workoutapp.databinding.ActivityExerciseBinding
+import com.workoutapp.databinding.DialogCustomBackConfirmationBinding
 import com.workoutapp.model.ExerciseModel
 import java.util.*
 import kotlin.collections.ArrayList
@@ -60,12 +62,34 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         tts = TextToSpeech(this, this)
 
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            onBackPressed()
+            customDialogForBackButton()
         }
 
         setupRestView()
 
         setupExerciseStatusRecyclerView()
+    }
+
+    override fun onBackPressed() {
+        customDialogForBackButton()
+    }
+
+    private fun customDialogForBackButton() {
+        val customDialog = Dialog(this)
+        val dialogBinding = DialogCustomBackConfirmationBinding.inflate(layoutInflater)
+
+        customDialog.setContentView(dialogBinding.root)
+        customDialog.setCanceledOnTouchOutside(false)
+
+        dialogBinding.btnYes.setOnClickListener {
+            this@ExerciseActivity.finish()
+            customDialog.dismiss()
+        }
+        dialogBinding.btnNo.setOnClickListener {
+            customDialog.dismiss()
+        }
+
+        customDialog.show()
     }
 
     private fun setupRestView() {
