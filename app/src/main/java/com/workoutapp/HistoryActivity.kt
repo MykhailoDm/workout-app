afcfd25eeff27b216101c85a2d233665a9cf3307
MykhailoDm <mikhailo2002dm@gmail.com>
2023-05-29 22:41:05 +0300

    Added history activity

---
+++ app/src/main/java/com/workoutapp/HistoryActivity.kt
@@ -0,0 +1,25 @@
+package com.workoutapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.workoutapp.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+
+    private var binding: ActivityHistoryBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarHistoryActivity)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "HISTORY"
+        }
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
